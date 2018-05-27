8a7f066d211b2887a7b1234be36089034b64011f
Edward <z5162357@cse.unsw.edu.au>
2018-05-28 02:32:02 +1000

    Updated newGraph, insertEdge and removeEdge functions

--- Graph.c
+++ Graph.c
@@ -1,87 +1,102 @@
 // Graph ADT implementation for Ass2 (COMP2521)
-/*
-typedef struct GraphRep *Graph;
-
-// vertices are ints
-typedef int Vertex;
-
-
-typedef struct _adjListNode {
-   Vertex         w;
-   int         weight;
-   struct _adjListNode *next;
-} adjListNode;
-
-typedef adjListNode* AdjList;
-
-*/
+//
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include "Graph.h"
 
-typedef struct _graph{
+typedef struct GraphRep{
     int nV;
     int nE;
     adjListNode **edges;
-} graph;
-Graph newGraph(int noNodes){
-    Graph new = malloc(sizeof(Graph));
-    new->nV = 0;
+} GraphRep;
+
+Graph newGraph(int noNodes) {
+
+    assert(noNodes != 0);
+    Graph new = malloc(sizeof(GraphRep));
+    new->nV = noNodes;
     new->nE = 0;
-    new->edges = malloc(noNodes*sizeof(adjListNode *))
-    for(int i = 0; i < noNodes, i++){
+    new->edges = malloc(noNodes*sizeof(adjListNode *));
+    for(int i = 0; i < noNodes; i++) {
         new->edges[i] = NULL;
     }
     return new;
 }
-void  insertEdge(Graph g, Vertex src, Vertex dest, int weight){
-    adjListNode *newEdge = malloc(sizeof(adjListNode));
+
+void insertEdge(Graph g, Vertex src, Vertex dest, int weight) {
+
+    assert(g != NULL && src >= 0 && src < g->nV);
+    AdjList newEdge = malloc(sizeof(adjListNode));
     newEdge->w = dest;
     newEdge->weight = weight;
     newEdge->next = NULL;
-    int i = 0;
-    while (g->edges[src][i]->next != NULL){
-        i++;
+    AdjList cur = g->edges[src];
+
+    if (cur == NULL) {
+        cur = newEdge;
     }
-    g->edges[src][i]->next = newEdge;
-}
-void  removeEdge(Graph g, Vertex src, Vertex dest){
-    //make src->next = dest->next
-    //remove src->next
-    int i = 0;
-    adjListNode *toRemove = NULL;
-    adjListNode *prev = NULL;
-    while (g->edges[src][i]->next != NULL){
-        if(g->edges[src][i]->next->w == dest){
-            bln = 1;
-            toRemove = g->edges[src][i]->next;
-            prev = g->edges[src][i];
-            break;
+
+    else {
+        while (cur->next != NULL) {
+            cur = cur->next;
         }
-        i++;
     }
-    if (bln == 1){
-        adjListNode *next = toRemove->next;
-        prev->next = next;
-        free(curr);
-    } else {
-        printf("error")
+    cur->next = newEdge;
+    g->nE++;
+}
+
+void removeEdge(Graph g, Vertex src, Vertex dest) {
+    AdjList cur = g->edges[src];
+
+    //Edge is at start of list
+
+    if (cur->w == dest) {
+        g->edges[src] = cur->next;
+        free(cur);
+        return;
     }
-
+    
+    //Edge is in middle/at end
+    
+    while (cur->next != NULL) {
+        if (cur->next->w == dest) {
+            AdjList tmp = cur->next;
+            cur->next = cur->next->next;
+            free(tmp);
+            return;
+        }
+        cur = cur->next;
+    }
 }
-bool  adjacent(Graph g, Vertex src, Vertex dest){
+bool adjacent(Graph g, Vertex src, Vertex dest) {
 
+    //TODO
+    return 0;
 }
-int  numVerticies(Graph g){
+int numVerticies(Graph g) {
     return g->nV;
 }
+
+//Returns a list of adjacent vertices on outgoing edges from a given vertex
 AdjList outIncident(Graph g, Vertex v){
 
-} //Returns a list of adjacent vertices on outgoing edges from a given vertex
+    //TODO
+    return 0;
+
+}
+
+//Returns a list of adjacent verticeson incoming edges from a given vertex.
 AdjList inIncident(Graph g, Vertex v){
 
-} //Returns a list of adjacent verticeson incoming edges from a given vertex.
+    //TODO
+    return 0;
+
+}
 void  showGraph(Graph g){
+    
+    //TODO
 
 }
 void  freeGraph(Graph g){
