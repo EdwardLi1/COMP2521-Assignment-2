0a9ec16b5cf21458963819d1214e13a36b76e6f3
Cary Guan <z5160345@student.unsw.edu.au>
2018-06-03 22:13:34 +1000

    commented and styled

--- Graph.c
+++ Graph.c
@@ -1,5 +1,5 @@
 // Graph ADT implementation for Ass2 (COMP2521)
-//
+// Made by Cary and Edward
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,43 +7,61 @@
 #include "Graph.h"
 
 typedef struct GraphRep{
-    int nV;
-    int nE;
-    adjListNode **edges;
+    int nV;         //Number of vertices in the graph
+    int nE;         //Number of edges in the graph
+    adjListNode **edges;        //Adjacency list of edges
 } GraphRep;
 
 Graph newGraph(int noNodes) {
-
     assert(noNodes != 0);
     Graph new = malloc(sizeof(GraphRep));
+    
+    //Check if malloc failed
+    if (new == NULL){
+        printf("Malloc Failed");
+        exit(1);
+    } 
+    
+    //Initialise values
     new->nV = noNodes;
     new->nE = 0;
     new->edges = malloc(noNodes*sizeof(adjListNode *));
+    
+    //Check if malloc failed
+    if (new->edges == NULL){
+        printf("Malloc Failed");
+        exit(1);
+    } 
     for(int i = 0; i < noNodes; i++) {
         new->edges[i] = NULL;
     }
+    
     return new;
 }
 
 void insertEdge(Graph g, Vertex src, Vertex dest, int weight) {
-
+    //Check for valid inputs
     assert(g != NULL && src >= 0 && src < g->nV);
     AdjList newEdge = malloc(sizeof(adjListNode));
+    
+    //Add create the new edge
     newEdge->w = dest;
     newEdge->weight = weight;
     newEdge->next = NULL;
     AdjList cur = g->edges[src];
-
+    
+    //Check if src has no edges
     if (cur == NULL) {
         g->edges[src] = newEdge;
     }
-
+    //Scan until the end of src's list
     else {
         while (cur->next != NULL) {
             cur = cur->next;
         }
         cur->next = newEdge;
     }
+    //Increment the number of edges in the graph
     g->nE++;
 }
 
@@ -51,7 +69,6 @@ void removeEdge(Graph g, Vertex src, Vertex dest) {
     AdjList cur = g->edges[src];
 
     //Edge is at start of list
-
     if (cur->w == dest) {
         g->edges[src] = cur->next;
         free(cur);
@@ -59,7 +76,6 @@ void removeEdge(Graph g, Vertex src, Vertex dest) {
     }
     
     //Edge is in middle/at end
-    
     while (cur->next != NULL) {
         if (cur->next->w == dest) {
             AdjList tmp = cur->next;
@@ -76,6 +92,7 @@ bool adjacent(Graph g, Vertex src, Vertex dest) {
     AdjList cur = g->edges[src];
     AdjList cur2 = g->edges[dest];
 
+    //Check if there is an edge from src to dest
     while (cur != NULL && isAdjacent == 0) {
         if (cur->w == dest) {
             isAdjacent = 1;
@@ -83,6 +100,7 @@ bool adjacent(Graph g, Vertex src, Vertex dest) {
         cur = cur->next;
     }
 
+    //Check if there is an edge from src to dest
     while (cur2 != NULL && isAdjacent == 0) {
         if (cur2->w == src) {
             isAdjacent = 1;
@@ -93,28 +111,34 @@ bool adjacent(Graph g, Vertex src, Vertex dest) {
     return isAdjacent;
 }
 int numVerticies(Graph g) {
+
     return g->nV;
 }
 
 //Returns a list of adjacent vertices on outgoing edges from a given vertex
 AdjList outIncident(Graph g, Vertex v){
+
     return g->edges[v];
 }
 
 //Returns a list of adjacent vertices on incoming edges from a given vertex.
 AdjList inIncident(Graph g, Vertex v){
 
+    //Create a new list to return
     AdjList head = NULL;
     AdjList tmp = NULL;
-
+    
+    //Iterate through every vertex to check if it has an edge to v
     for (int i = 0; i < g->nV; i++) {
         AdjList cur = g->edges[i];
         while (cur != NULL) {
+            //If an edge exists create a new node and add it to our created list
             if (cur->w == v) {
                 AdjList new = malloc(sizeof(adjListNode));
                 new->w = i;
                 new->weight = cur->weight;
                 new->next = NULL;
+                //empty list case
                 if (head == NULL) {
                     head = new;
                     tmp = new;
@@ -132,10 +156,14 @@ AdjList inIncident(Graph g, Vertex v){
 
 }
 void showGraph(Graph g) {
+    
+    //Print out all edges from each vertex in a line
     for (int i = 0; i < g->nV; i++) {
         AdjList cur = g->edges[i];
+        //Initial statement
         printf("[%d] has an edge to [", i);
         while (cur != NULL) {
+            //All edges 
             if (cur->next == NULL) {
                 printf("%d", cur->w);
             }
@@ -149,7 +177,8 @@ void showGraph(Graph g) {
 }
 
 void freeGraph(Graph g){
-    
+
+    //Free each node of the list at a time
     for (int i = 0; i < g->nV; i++) {
         AdjList cur = g->edges[i];
         while (cur != NULL) {
@@ -158,6 +187,7 @@ void freeGraph(Graph g){
             free(tmp);
         }
     }
+    //Free graph
     free(g);
 }
 
--- PQ.c
+++ PQ.c
@@ -6,48 +6,57 @@
 #include <math.h>
 #define INIT_HEAPSIZE 2
 
-typedef struct PQRep {
+typedef struct PQRep {  //Heap implementation
 	ItemPQ *items;
-	int nslots;     //how many total slots we have
-	int size;       //how many were using rn
+	int nslots;     //How many array indexes we have malloc'd
+	int size;       //How many relevant array entries we have
 } PQRep;
 
-void fixUp(ItemPQ a[], int i);
-void swap(ItemPQ a[], int i, int j);
-int larger(ItemPQ a, ItemPQ b);
-void fixDown(ItemPQ a[], int i, int N);
-void arrayCopy(ItemPQ *array1, ItemPQ *array2, int size);
-// Creates new priority queue, that can store items of type ItemPQ.
+//helper function prototyes
+static void fixUp(ItemPQ a[], int i);                                       //Restores priority after inserting an element at the end of the list
+static void swap(ItemPQ a[], int i, int j);                                 //Swaps 2 items
+static int larger(ItemPQ a, ItemPQ b);                                      //Checks if a is larger than b
+static void fixDown(ItemPQ a[], int i, int N);                              //Restores priority after removing an element from the start of the list
+static void arrayCopy(ItemPQ *array1, ItemPQ *array2, int size);            //Copies array1 into array2. Size is the size of array1
 
+// Creates new priority queue, that can store items of type ItemPQ
 PQ newPQ(){
-    PQ new = (PQRep *)malloc(sizeof(PQRep));
+
+    //Allocate space for a priority queue and check for malloc errors
+    PQ new = malloc(sizeof(PQRep));
     if (new == NULL){
         printf("Malloc Failed");
         exit(1);
     }
-
+    
+    //Allocate space for a ItemPQ list and check for malloc errors
     ItemPQ *newItems = malloc(INIT_HEAPSIZE*sizeof(ItemPQ));
     if (newItems == NULL){
         printf("Malloc Failed");
         exit(1);
     }
-
+    
+    //Initialise values of a empty PQ
     new->items = newItems;
     new->size = 0; 
     new->nslots = INIT_HEAPSIZE;
+    
     return new;
 }
 
 int PQEmpty(PQ p) {
+
+    //Check if the size element is 0 or if the queue is NULL
 	if (p->size == 0 || p == NULL){
 	    return 1;
 	}
+	
 	return 0;
 }
 
 void  addPQ(PQ pq, ItemPQ item){
-    //check if item with key exists
-    //if it does then update its value
+
+    //Check if an item with key exists. If it does then update its value
     int bln = -1;
     for(int i = 1; i <= pq->size; i++){
         if (pq->items[i].key == item.key){
@@ -56,63 +65,97 @@ void  addPQ(PQ pq, ItemPQ item){
             break; 
         }
     }
-    if (bln == -1){         //this indicates that we did not find an item with the existing key
+    
+    //Check if we found an item with pre-existing key
+    if (bln == -1){
+        //Check if we have run out of memory in our ItemPQ list
         if (pq->size == pq->nslots -1){
             pq->nslots++;
-            ItemPQ *newItems = malloc(pq->nslots*sizeof(ItemPQ));            
+            
+            //Create a new list and check for malloc errors
+            ItemPQ *newItems = malloc(pq->nslots*sizeof(ItemPQ));  
+            if (newItems == NULL){
+                printf("Malloc Failed");
+                exit(1);
+            }    
+                  
+            //Call helper function to copy original list into new one
             arrayCopy(pq->items, newItems, pq->size);
+            free(pq->items);
+            
+            //Assign new list as the ItemPQ list for PQ
             pq->items = newItems;
         }
+        
+        //Insert new item
         assert(pq->size < pq->nslots);
         pq->size++;
         pq->items[pq->size] = item;
+        
+        //Fix the queue so that the parent's priority is always higher than its children
         fixUp(pq->items, pq->size);
     }
      
 }
 
 ItemPQ dequeuePQ(PQ pq) {
+
+    //Remove the first item
     ItemPQ remove = pq->items[1];
+    
+    //Move last item to the front
     pq->items[1] = pq->items[pq->size];
     pq->size--;
+    
+    //Fix the queue so that the parent's priority is always higher than its children
     fixDown(pq->items, 1, pq->size);
+    
 	return remove;
 }
 
 void updatePQ(PQ pq, ItemPQ element) {
+
     int i = 0;
+    //Iterate through the list and look for they key
     while (i <= pq->size){
         if (pq->items[i].key == element.key){
             pq->items[i].value = element.value;
+            //Ensure that priority is maintained
             fixUp(pq->items, pq->size);
         }
         i++;
     }
 }
 
-void  showPQ(PQ pq) {
+void showPQ(PQ pq) {
+
+    //Create a counter to detect powers of 2
     int i = 1;
     int j = 0;
     int count = 0;
     while (i < pq->nslots && i <= pq->size){
         double power = pow(2,j);
+        
+        //Print an item's key
         printf("%d ", pq->items[i].key);
         count++;
+        
+        //If we have printed 2 times the number of items than the previous line then print a new line
         if (count == power){
             printf("\n");
             count = 0;
             j++;
         }
         i++;
-        
     }
+    
     printf("\n");
     printf("\n");
     
-    
-     i = 1;
-     j = 0;
-     count = 0;
+    //Repeat as above but this time printing the item's value
+    i = 1;
+    j = 0;
+    count = 0;
     while (i < pq->nslots && i <= pq->size){
         double power = pow(2,j);
         printf("%d ", pq->items[i].value);
@@ -129,42 +172,65 @@ void  showPQ(PQ pq) {
 }
 
 void  freePQ(PQ pq){
-    //for (int i = 0; i <= pq->size; i++){
-    //    free(pq->items[i]);
-    //}
     free(pq->items);
     free(pq);
 }
-//helper functions fixup, fix down and swap are based off week11 lectures notes from COMP2521
-void fixUp(ItemPQ a[], int i){
+
+//----------------------------------------------------------------------------------------------------------
+//      HELPER FUNCTIONS
+// Helper functions fixup, fix down and swap are based off week11 lectures notes from COMP2521. They are edited and adapted to fit our requirements
+//----------------------------------------------------------------------------------------------------------
+
+
+static void fixUp(ItemPQ a[], int i){
+
+    //If a child is smaller than its parent then swap them
     while (i > 1 && larger(a[i/2],a[i]) == 1) {
         swap(a, i, i/2);
         i = i/2;
     }
 }
-void fixDown(ItemPQ a[], int i, int N){
+
+static void fixDown(ItemPQ a[], int i, int N){
+
     while (2*i <= N) {
-        int j = 2*i;          //j stores the index of the left child
-        if (j < N && larger(a[j], a[j+1]) == 1) j++;     //j store index of the smaller of the 2 children
+    
+        //Use int as it stores the floor of a double value
+        int j = 2*i;       
+        
+        //Find the smaller of the two children   
+        if (j < N && larger(a[j], a[j+1]) == 1) j++;
+        
+        //If the parent is smaller than the children then priority is restored
         if (larger(a[i], a[j]) == 0 && a[i].value != a[j].value) break;
+        
+        //Else the parent and smallest child swaps
         swap(a, i, j);
         i = j;
     }
 }
-void swap(ItemPQ a[], int i, int j){
+
+static void swap(ItemPQ a[], int i, int j){
+
     ItemPQ tmp = a[i];
     a[i] = a[j];
     a[j] = tmp;
 }
-int larger(ItemPQ a, ItemPQ b){
+
+static int larger(ItemPQ a, ItemPQ b){
+
     if (a.value > b.value){
         return 1;
     } else {
         return 0;
     }
 }
-void arrayCopy(ItemPQ *array1, ItemPQ *array2, int size){     //copy contents of array1 into array2
+
+static void arrayCopy(ItemPQ *array1, ItemPQ *array2, int size){
+
     int i = 1;
+    
+    //Copy values in array1 into array2 one at a time
     while (i <= size){
         array2[i] = array1[i];
         i++;
